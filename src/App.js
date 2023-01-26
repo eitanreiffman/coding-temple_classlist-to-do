fe7c262853f442e2624034a8ff5fdd66c80841fe
Eitan Reiffman <eitanreiffman@gmail.com>
2023-01-25 23:04:10 -0500

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ClassList from './components/ClassList';
+import Navbar from './components/Navbar';
 import ToDo from './components/ToDo';
 
 export default function App(){
     return (
         <>
+            <Navbar />
             <div className='container'>
                 <ToDo />
                 <ClassList />
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Navbar(props) {
+  return (
+    <nav className='navbar bg-secondary'>
+        <div className="container-fluid">
+            <span className="navbar-brand fs-3 fw-bold">To-Do List and Kekambas Class List</span>
+        </div>
+    </nav>
+  )
+}
